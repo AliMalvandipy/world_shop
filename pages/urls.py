03b4da9cc83da0071efdc70c36f33e2ed95085c0
Ali Malvandi <malvandi.py@gmail.com>
2024-10-03 17:52:21 +0330

    add messages framrke

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
     path('about/', AboutUsPage.as_view(), name='about'  ),
+    path('test/', test, name='test'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from django.contrib import messages
 class HomePage(TemplateView):
     template_name = 'home.html'
+    
 
 class AboutUsPage(TemplateView):
     template_name = 'pages/about.html'
+
+def test (request):
+    result = 'hello world'
+    messages.success(request, 'success')
+    return render(request, 'pages/test.html')
+
